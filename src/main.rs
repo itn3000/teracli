248816065d7a27e8bcdab38423f9a1750a1f0e7a
itn3000 <itn3000@gmail.com>
2020-12-28 11:22:56 +0900

    read from stdin feature added, add test files

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,74 @@
-use std::io::Read;
-use serde_json::Value;
-const OPTION_STRING: &str = "i:p:o:";
+use std::io::{ Write, Read };
+
+use clap::ArgMatches;
+
 fn create_app<'a>() -> clap::App<'a> {
     clap::App::new("teracli")
         .arg(
             clap::Arg::new("input")
-                .takes_value(true)
+                .value_name("INPUT_FILE")
                 .long("input")
                 .short('i')
-                .required(true)
         )
         .arg(
             clap::Arg::new("parameter_file")
                 .long("parameter")
                 .short('p')
+                .value_name("PARAMETER_JSON")
                 .required(true)
+        )
+        .arg(
+            clap::Arg::new("output_file")
+                .long("output")
+                .short('o')
+                .value_name("OUTPUT_FILE")
         )
 }
 
 fn create_tera_context_from_parameter(json_path: &str) -> anyhow::Result<tera::Context> {
-    let mut context = tera::Context::new();
     let f = std::fs::File::open(json_path)?;
     let parsed: serde_json::Value = serde_json::from_reader(f)?;
-    match parsed {
-        serde_json::Value::Object(v) => {
-
-        }
-    };
+    // let context = create_tera_context_from_jsonvalue(&parsed);
+    let context = tera::Context::from_serialize(parsed)?;
     Ok(context)
 }
 
-fn create_tera_context_from_jsonvalue(value: &Value) -> tera::Context {
-    let context = tera::Context::new();
-    context.extend()
-    match value {
-        Value::Object(v) => {
-            for k in v.keys() {
-                match v[k] {
-                    Value::Object(inner) => 
-                        context.insert(k, &create_tera_context_from_jsonvalue(&Value::Object(inner))),
-                    Value::String(inner) => context.insert(k, &inner),
-                    Value::Number(inner) => context.insert(k, &inner),
-                    Value::Array(inner) => context.insert(k, &inner),
-                    Value::Bool(inner) => context.insert(k, &inner),
-                    Value::Null => (),
-                };
-            }
+fn get_input_from_matches(matches: &clap::ArgMatches) -> anyhow::Result<String> {
+    Ok(match matches.value_of("input") {
+        Some(v) => {
+            let mut fi = std::fs::File::open(v)?;
+            let mut ret = String::new();
+            fi.read_to_string(&mut ret)?;
+            ret
         },
-        _ 
-    };
-    context
+        None => {
+            let mut si = std::io::stdin();
+            let mut ret = String::new();
+            si.read_to_string(&mut ret)?;
+            ret
+        }
+    })
 }
 
 fn main() -> anyhow::Result<()> {
     let app = create_app();
     let matches = app.get_matches();
-    let input_file = matches.value_of("input").unwrap();
     let parameter_file = matches.value_of("parameter_file").unwrap();
-
+    let context = create_tera_context_from_parameter(parameter_file)?;
+    let mut tr = tera::Tera::default();
+    let input_string = get_input_from_matches(&matches)?;
+    let parsed = tr.render_str(&input_string, &context)?;
+    match matches.value_of("output") {
+        Some(v) => {
+            let mut o = std::fs::File::create(v)?;
+            let bytes = parsed.as_bytes();
+            o.write(&bytes)?;
+        },
+        None => {
+            let mut so = std::io::stdout();
+            let bytes = parsed.as_bytes();
+            so.write(&bytes)?;
+        }
+    };
     Ok(())
 }
